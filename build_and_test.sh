7923aa5fa8be35af2bd4f88324f9397b672c0e5d
Marton Hajdu <Marton.Hajdu@logmein.com>
2018-01-30 22:04:57 +0100

    Add third test

--- build_and_test.sh
+++ build_and_test.sh
@@ -4,32 +4,24 @@ bazel build //:sokohard
 
 readonly SEED2x2w3="1517341101"
 readonly SEED3x3w2="1517341110"
+readonly SEED2x2w5="1517341640"
+
 time bazel-bin/sokohard --width 2 --height 2 --numBoxes 3 --seed ${SEED2x2w3}
 time bazel-bin/sokohard --width 3 --height 3 --numBoxes 2 --seed ${SEED3x3w2}
+time bazel-bin/sokohard --width 2 --height 2 --numBoxes 5 --seed ${SEED2x2w5}
 
-#Random seed is 1517341101
-#Generating level....................................................................................................finished!
-#
-#Size: 6x6
-#Number of boxes: 3
-#Difficulty (measured as the sum of
-#box lines and box changes): 20
-#
-#Writing result map to file...completed!
-#
+#1517341101, 6x6, boxes: 3
+#Difficulty: 20
 #real	0m9.936s
 #user	0m9.705s
 #sys	0m0.079s
-#Random seed is 1517341110
-#Generating level....................................................................................................finished!
-#
-#Size: 9x9
-#Number of boxes: 2
-#Difficulty (measured as the sum of
-#box lines and box changes): 21
-#
-#Writing result map to file...completed!
-#
+#1517341110, 9x9, boxes: 2
+#Difficulty: 21
 #real	0m17.667s
 #user	0m17.290s
 #sys	0m0.117s
+#1517341640, 6x6, boxes: 5
+#Difficulty: 49
+#real	0m45.234s
+#user	0m44.290s
+#sys	0m0.304s
