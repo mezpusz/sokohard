8ed0a5c050cd88953bd0c11ceca7c4335df33d46
Marton Hajdu <Marton.Hajdu@logmein.com>
2018-02-17 12:34:46 +0100

    Add test files and check for diff in generated map and solution as well

--- build_and_test.sh
+++ build_and_test.sh
@@ -8,12 +8,24 @@ readonly SEED2x2w3="1517341101"
 readonly SEED3x3w2="1517341110"
 readonly SEED2x2w5="1517341640"
 
-time out/sokohard -w 2 -h 2 -b 3 -s ${SEED2x2w3}               # 14
-time out/sokohard -w 2 -h 2 -b 3 -s ${SEED2x2w3} --box-changes # 24
-time out/sokohard -w 3 -h 3 -b 2 -s ${SEED3x3w2}               # 13
-time out/sokohard -w 3 -h 3 -b 2 -s ${SEED3x3w2} --box-changes # 40
-time out/sokohard -w 2 -h 2 -b 5 -s ${SEED2x2w5}               # 22
-time out/sokohard -w 2 -h 2 -b 5 -s ${SEED2x2w5} --box-changes # 35
+time out/sokohard -w 2 -h 2 -b 3 -s ${SEED2x2w3} -o "2x2w3lines"                 # 14
+diff 2x2w3lines.sok testfiles/2x2w3lines.sok
+diff 2x2w3lines_solution.sok testfiles/2x2w3lines_solution.sok
+time out/sokohard -w 2 -h 2 -b 3 -s ${SEED2x2w3} --box-changes -o "2x2w3changes" # 24
+diff 2x2w3changes.sok testfiles/2x2w3changes.sok
+diff 2x2w3changes_solution.sok testfiles/2x2w3changes_solution.sok
+time out/sokohard -w 3 -h 3 -b 2 -s ${SEED3x3w2} -o "3x3w2lines"                 # 13
+diff 3x3w2lines.sok testfiles/3x3w2lines.sok
+diff 3x3w2lines_solution.sok testfiles/3x3w2lines_solution.sok
+time out/sokohard -w 3 -h 3 -b 2 -s ${SEED3x3w2} --box-changes -o "3x3w2changes" # 40
+diff 3x3w2changes.sok testfiles/3x3w2changes.sok
+diff 3x3w2changes_solution.sok testfiles/3x3w2changes_solution.sok
+time out/sokohard -w 2 -h 2 -b 5 -s ${SEED2x2w5} -o "2x2w5lines"                 # 22
+diff 2x2w5lines.sok testfiles/2x2w5lines.sok
+diff 2x2w5lines_solution.sok testfiles/2x2w5lines_solution.sok
+time out/sokohard -w 2 -h 2 -b 5 -s ${SEED2x2w5} --box-changes -o "2x2w5changes" # 35
+diff 2x2w5changes.sok testfiles/2x2w5changes.sok
+diff 2x2w5changes_solution.sok testfiles/2x2w5changes_solution.sok
 
 # current bests: 14, 13, 22, 35
 
